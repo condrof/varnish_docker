cbe7de51eface5f5b4d3bde0f4c769cfd0fa4392
Fergal Condron <condrof@gmail.com>
2016-07-12 15:26:03 +0100

    more default.vcl

--- start-varnish.sh
+++ start-varnish.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-for name in BACKEND_PORT_80_TCP_ADDR
+for name in BACKEND_PORT
 do
-eval value=\$$name
-sed "s|\${${name}}|${value}|g" /etc/varnish/default.templ > /etc/varnish/default.vcl
+  eval value=\$$name
+  sed -ie "s|\${${name}}|${value}|g" /etc/varnish/default.templ
+  sed "s|\${${name}}|${value}|g" /etc/varnish/default.templ > /etc/varnish/default.vcl
 done
+
+for name in BACKEND_PORT
+do
+  eval value=\$$name
+  sed "s|\${${name}}|${value}|g" /etc/varnish/default.templ > /etc/varnish/default.vcl
+done
+
+cat /etc/varnish/default.vcl
 varnishd -F -f /etc/varnish/default.vcl -s malloc,$CACHE_SIZE -a 0.0.0.0:80
